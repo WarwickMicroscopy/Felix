acdb4e986dc447f1a46ec273321e650cd476a5d8
Alex Simpson <a.simpson.1@warwick.ac.uk>
2015-08-21 16:34:27 +0100

    Fixed bugs with CIF loading at runtime and also editted the wiki

--- src/GUI/FileCtrl.py
+++ src/GUI/FileCtrl.py
@@ -1,6 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+# felixsim
+#
+# Richard Beanland, Keith Evans, Rudolf A Roemer and Alexander Hubert
+#
+# (C) 2013/14, all right reserved
+#
+# Version: :VERSION:
+# Date:    :DATE:
+# Time:    :TIME:
+# Status:  :RLSTATUS:
+# Build:   :BUILD:
+# Author:  :AUTHOR:
+#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+#  This file is part of felixsim.
+#
+#  felixsim is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  felixsim is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with felixsim.  If not, see <http://www.gnu.org/licenses/>.
+#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 import wx
 import wx.lib.agw.floatspin as FS
 import math
@@ -42,10 +76,10 @@ def RunFelix(parent, CIFPath, OutputPath):
                   wx.OK | wx.ICON_ERROR)
     OnCif(parent)
 
-  if os.path.exists(CIFPath) == False:
+  if CIFPath !=None and os.path.exists(CIFPath) == False:
     wx.MessageBox('.cif file not found, please load a .cif file', 'Error',
                   wx.OK | wx.ICON_ERROR)
-    OnCif(parent)
+    CIFPath = OnCif(parent)
 
   # creates working directory
   cpath, cfilename = os.path.split(CIFPath)
--- src/GUI/wikistrings.py
+++ src/GUI/wikistrings.py
@@ -1,6 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+# felixsim
+#
+# Richard Beanland, Keith Evans, Rudolf A Roemer and Alexander Hubert
+#
+# (C) 2013/14, all right reserved
+#
+# Version: :VERSION:
+# Date:    :DATE:
+# Time:    :TIME:
+# Status:  :RLSTATUS:
+# Build:   :BUILD:
+# Author:  :AUTHOR:
+#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+#  This file is part of felixsim.
+#
+#  felixsim is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  felixsim is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with felixsim.  If not, see <http://www.gnu.org/licenses/>.
+#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 DefaultWiki = r"""
 <p><em>Felix</em> is a Bloch wave simulation code, specifically for D-LACBED diffraction patterns. Written and compiled using Fortran 95.</p>
 
@@ -287,8 +321,6 @@ IReflectOutWiki = r"""
 <p><strong>IReflectOut</strong></p>
 
 <p>The number of the reflections to be included in the final image(s)</p>
-
-<p><strong><em>The remaining parameters apply to FelixRefine only</em></strong></p>
 """
 
 IImageOutputFLAGWiki  = r"""
