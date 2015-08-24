a5f43bae8cd575908ae48dbaeab834f0b02967dc
Alex Simpson <a.simpson.1@warwick.ac.uk>
2015-08-24 12:11:34 +0100

    Updated setup file

--- src/GUI/setup.py
+++ src/GUI/setup.py
@@ -1,6 +1,9 @@
 import sys
 from cx_Freeze import setup, Executable
 
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {}
+
 # GUI applications require a different base on Windows (the default is for a
 # console application).
 base = None
@@ -9,6 +12,6 @@ if sys.platform == "win32":
 
 setup(  name = "FelixGUI",
         version = "0.1",
-        description = "Felixsim GUI Application",
+        description = "GUI for Felixsim suite of electron diffraction codes",
         options = {"build_exe": build_exe_options},
         executables = [Executable("Felix_gui.py", base=base)])
