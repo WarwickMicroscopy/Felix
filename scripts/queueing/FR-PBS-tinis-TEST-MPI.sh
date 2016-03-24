bee37d4b6dc495ad863387ad5ef512f855b43ade
Richard Beanland <phshas@login1.tinis.cluster>
2016-03-24 11:03:45 +0000

    with tinis makefile and run script

---
+++ scripts/queueing/FR-PBS-tinis-TEST-MPI.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -l nodes=2:ppn=12
+#PBS -l pmem=2012mb
+#PBS -l walltime=00:30:00
+#PBS -M $USER@warwick.ac.uk
+#PBS -m a
+#PBS -r y
+#PBS -V
+#PBS -k oe
+#PBS -j oe
+
+module load intel impi imkl FFTW
+
+pwd
+cd ~/Felix/samples/FR-GaAs/
+pwd
+ls
+srun -n 24 ../../src/felixrefine
