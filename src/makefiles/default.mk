5f7c36ae5c07a7905c66d25144c6f1f06fd711ce
RudoRoemer <r.roemer@warwick.ac.uk>
2017-08-11 11:16:57 +0100

    new make structure that compiles and makes libraries notyet linking

--- src/makefiles/default.mk
+++ src/makefiles/default.mk
@@ -7,6 +7,7 @@ MAIN=felixrefine
 FELIX=\
 $(DIRFELIX)$(PRECISION)gmodules.o \
 $(DIRFELIX)$(PRECISION)smodules.o \
+$(DIRFELIX)$(PRECISION)message_mod.o \
 $(DIRFELIX)$(PRECISION)felixrefine.o \
 $(DIRFELIX)$(PRECISION)felixfunction.o \
 $(DIRFELIX)$(PRECISION)bloch.o \
@@ -21,7 +22,6 @@ $(DIRFELIX)$(PRECISION)in.o \
 $(DIRFELIX)$(PRECISION)scatteringfactors.o \
 $(DIRFELIX)$(PRECISION)writeoutput.o \
 $(DIRFELIX)$(PRECISION)errorchecks.o \
-$(DIRFELIX)$(PRECISION)message_mod.o \
 $(DIRFELIX)$(PRECISION)simplex.o \
 $(DIRFELIX)$(PRECISION)readcif.o \
 $(DIRFELIX)$(PRECISION)symmetry.o
@@ -31,12 +31,12 @@ QUADPACK= \
   $(DIRQUADPACK)$(PRECISION)quadpack_double.o
 
 CIFTBX=\
-$(DIRCIFTBX)$(PRECISION)ciftbx.o $(DIRCIFTBX)hash_funcs.o
+$(DIRCIFTBX)$(PRECISION)ciftbx.o $(DIRCIFTBX)$(PRECISION)hash_funcs.o
 
 SAMPLES=$(MAIN).o
 
-STARTDIR=$(PWD)
-MYSTARTDIR=$(STARTDIR)
+#STARTDIR=$(PWD)
+#MYSTARTDIR=$(STARTDIR)
 
 # Linux
 #LIBS=-ljadamilu  -llapack -lblas -lm -lc 
@@ -56,8 +56,11 @@ MYSTARTDIR=$(STARTDIR)
 INCDIR=$(MYSTARTDIR)/include
 
 # where are the libraries
-LIBDIR=$(MYSTARTDIR)/lib/$(MYPLATFORM)
+LIBDIR=$(STARTDIR)/../lib/$(PLATFORM)
 
+LIBFELIX=/lib$(PRECISION)felix.a
+LIBQUADPACK=lib$(PRECISION)quadpack.a
+LIBCIFTBX=lib$(PRECISION)ciftbx.a
 
 .SUFFIXES: .c .f .f90 .F .o .a
 .DEFAULT: main
@@ -69,17 +72,24 @@ LIBDIR=$(MYSTARTDIR)/lib/$(MYPLATFORM)
 #%.o: %.F
 #	$(FCOMPILE)
 
-$(PRECISION)%.o: %.f %.f90
-	@ echo --- --- compiling fortran code
-	$(FF)  $(FFFLAGS)  -c -o $(PRECISION)$@ $<
+$(PRECISION)%.o: %.f90
+#	@ echo --- --- compiling F90+ code
+	$(F90) $(F90FLAGS) -c -o $@ $<
 
+$(PRECISION)%.o: %.f
+#	@ echo --- --- compiling F77 code
+	$(FF) $(FFFLAGS) -c -o $@ $<
 
 $(PRECISION)%.o: %.c
 	$(CC)  $(CCFLAGS)  -I$(INCDIR)  $(FORTRANNAMES) $(ARITHMETIC) $(LONGINTEGER) -c -o $@ $<
 
-
 $(PRECISION)%.o: %.F
 	$(FCOMPILE)
 
+#exe: $(STARTDIR)/$(MAIN).$(PLATFORM).$(PRECISION)
+
+#$(STARTDIR)/$(MAIN).$(PLATFORM).$(PRECISION): $(FELIX)
+#	@ echo --- linking $(MAIN) executable
+#	$(LD) $(LDFLAGS) -o $@ $(FELIX) -L$(LIBDIR) -l$(LIBQUADPACK)  -l$(LIBCIFTBX)
 
 
