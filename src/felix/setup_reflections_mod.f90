dfcadfeb4c467ca69add33311cdf00ab644a59e8
rbeanland <r.beanland@warwick.ac.uk>
2021-08-04 11:30:38 +0100

    mproved bem selection criteria

--- src/felix/setup_reflections_mod.f90
+++ src/felix/setup_reflections_mod.f90
@@ -180,19 +180,20 @@ MODULE setup_reflections_mod
     USE message_mod
     
     ! global output Rhkl, input reciprocal lattice vectors & wave vector
-    USE RPARA, ONLY : RzDirC,Rhkl,RarVecM,RbrVecM,RcrVecM,RElectronWaveVectorMagnitude
+    USE RPARA, ONLY : RzDirC, Rhkl, RarVecM, RbrVecM, RcrVecM, RInputHKLs,&
+        RElectronWaveVectorMagnitude
       
     ! global inputs
     USE SPARA, ONLY : SSpaceGroupName
-    USE IPARA, ONLY : INhkl
+    USE IPARA, ONLY : INhkl,INoOfLacbedPatterns
     USE Iconst
     
     IMPLICIT NONE
 
-    REAL(RKIND),INTENT(IN) :: RGlimit!RTol   
-    INTEGER(IKIND) :: IErr, ISel, Ih, Ik, Il, inda,indb,indc, jnda,jndb,jndc, knd,lnd
-    REAL(RKIND) :: RarMag, RbrMag, RcrMag, RGtestMag, RGpluskMag, RShell
-    REAL(RKIND),DIMENSION(ITHREE) :: RGtestM, RGtest 
+    REAL(RKIND),INTENT(IN) :: RGlimit   
+    INTEGER(IKIND) :: IErr, ISel, Ih, Ik, Il, inda,indb,indc, jnd, knd,lnd
+    REAL(RKIND) :: RarMag, RbrMag, RcrMag, RShell, RGtestMag, RDev
+    REAL(RKIND),DIMENSION(ITHREE) :: Rk, RGtest, RGtestM, RGplusk 
    
     !The upper limit for g-vector magnitudes
     !If the g-vectors we are counting are bigger than this there is something wrong
@@ -202,50 +203,58 @@ MODULE setup_reflections_mod
     
     !the k-vector for the incident beam
     !we are working in the microscope reference frame so k is along z
-!    Rk=(/ 0.0,0.0,RElectronWaveVectorMagnitude /)
-    
+    Rk=(/ 0.0,0.0,RElectronWaveVectorMagnitude /)
     !get the size of the reciprocal lattice basis vectors
     RarMag=SQRT(DOT_PRODUCT(RarVecM,RarVecM))!magnitude of a*
     RbrMag=SQRT(DOT_PRODUCT(RbrVecM,RbrVecM))!magnitude of b*
     RcrMag=SQRT(DOT_PRODUCT(RcrVecM,RcrVecM))!magnitude of c*
-    !limit the shell increment by the smallest basis vector
+    !we work our way out from the origin in shells
+    !the shell increment is the smallest basis vector
     RShell=MINVAL( (/ RarMag,RbrMag,RcrMag /) )
 
-    !we work our way out from the origin in shells
-    Rhkl(1,:)=(/ 0.0,0.0,0.0 /)! first g is always 000
-    knd=1!current number of reflections in the pool 
-    lnd=0!current number of times we've expanded the search 
-    ! first shell
-    RGtestMag=0.0
+    !first g is always 000
+    Rhkl(1,:)=(/ 0.0,0.0,0.0 /)
+    knd=1!number of reflections in the pool 
+    lnd=0!number of the shell 
     !maximum a*,b*,c* limit is determined by the G magnitude limit
     inda=NINT(RGlimit/RarMag)
     indb=NINT(RGlimit/RbrMag)
     indc=NINT(RGlimit/RcrMag)
+    !fill the Rhkl with beams near the Bragg condition
     DO WHILE (knd.LT.INhkl .AND. REAL(lnd)*RShell.LT.RGlimit)
-      lnd=lnd+1
+      !increment the shell
+      lnd = lnd+1
 !DBG    IF(my_rank.EQ.0)PRINT*,REAL(lnd-1)*RShell,"to",REAL(lnd)*RShell
-      DO Ih=-inda,inda
-         DO Ik=-indb,indb
-            DO Il=-indc,indc
-              !Make a g-vector hkl
-              RGtest=REAL( (/ Ih,Ik,Il /),RKIND )
-              RGtestM=REAL(Ih)*RarVecM+REAL(Ik)*RbrVecM+REAL(Il)*RcrVecM
-              RGtestMag=SQRT(DOT_PRODUCT(RGtestM,RGtestM))
-              !is it in the shell
-              IF (RGtestMag.GT.REAL(lnd-1)*RShell.AND.RGtestMag.LE.REAL(lnd)*RShell) THEN
-!DBG    IF(my_rank.EQ.0)PRINT*,NINT(RGtest),RGtestMag
-!DBG    IF(my_rank.EQ.0)PRINT*,"in shell",lnd
-                IF (ABS(DOT_PRODUCT(RGtest,RzDirC)).LT.TINY) THEN !it's in the ZOLZ 
-                 !check that it's allowed by selection rules
-                  ISel=0
-                  CALL SelectionRules(Ih, Ik, Il, ISel, IErr)
-                  !add it to the pool and increment the counter
-                  !need to check that we have space in Rhkl because the while doesn't
-                  !kick in until we finish the do loops
-                  IF (ISel.EQ.1 .AND. knd.LT.INhkl) THEN
+      !Make a hkl
+      DO Ih = -inda,inda
+         DO Ik = -indb,indb
+            DO Il = -indc,indc
+              !check that it's allowed by selection rules
+              ISel=0
+              CALL SelectionRules(Ih, Ik, Il, ISel, IErr)
+              !NB still need to check that we have space in Rhkl because the
+              !while doesn't kick in until we finish the do loops
+              IF (ISel.EQ.1 .AND. knd.LT.INhkl) THEN
+                !Make a g-vector
+                RGtest = REAL( (/ Ih,Ik,Il /),RKIND )!Miller indices
+                RGtestM = REAL(Ih)*RarVecM+REAL(Ik)*RbrVecM+REAL(Il)*RcrVecM!in microscope frame
+                RGtestMag = SQRT(DOT_PRODUCT(RGtestM,RGtestM))
+                !is it in the shell
+                IF (RGtestMag.GT.REAL(lnd-1)*RShell.AND.RGtestMag.LE.REAL(lnd)*RShell) THEN
+                  !is it near a Laue condition |k+g|=|k|
+                  RGplusk=RGtestM+Rk
+                  !divide by |g| to get a measure independent of incident beam tilt
+                  RDev=ABS(RElectronWaveVectorMagnitude-SQRT(DOT_PRODUCT(RGplusk,RGplusk)))/RGtestMag
+                  ! Tolerance of 0.08 here is rather arbitrary, might need
+                  ! revisiting
+!DBG    IF(my_rank.EQ.0)PRINT*,(/ Ih,Ik,Il /),RDev
+                  IF ((RDev-0.08).LT.TINY) THEN !it's near the ZOLZ 
+                    !add it to the pool and increment the counter
+                    !need to check that we have space in Rhkl because the while doesn't
+                    !kick in until we finish the do loops
                     knd=knd+1
                     Rhkl(knd,:)=RGtest
-!DBG    IF(my_rank.EQ.0)PRINT*,NINT(Rhkl(knd,:)),-Ih+Ik+Il
+!DBG    IF(my_rank.EQ.0)PRINT*,NINT(Rhkl(knd,:)),knd
                   END IF
                 END IF
               END IF
@@ -254,6 +263,25 @@ MODULE setup_reflections_mod
       END DO
     END DO
 !DBG    IF(my_rank.EQ.0)PRINT*,"total ",knd,"reflections in the pool"
+    ! fill up any remaining beam pool places with an enormous g-vector, diabolically
+    ! the idea being that this g-vector will never be near any possible Ewald sphere
+    RGtest = REAL( (/ 666,666,666 /),RKIND )
+    DO jnd = knd+1, INhkl
+      Rhkl(jnd,:)=RGtest
+    END DO
+
+    !now check that the required output hkl's are in this hkl list
+    DO jnd = 1,INoOfLacbedPatterns
+      lnd = 0!using this as a flag now
+      DO knd = 1, INhkl
+        IF (RInputHKLs(jnd,1)-Rhkl(knd,1).LT.TINY .AND. &
+            RInputHKLs(jnd,2)-Rhkl(knd,2).LT.TINY .AND. &
+            RInputHKLs(jnd,3)-Rhkl(knd,3).LT.TINY) lnd = 1
+      END DO
+      IF (lnd.EQ.0) THEN
+        CALL message( LS, "Input hkl not found: ", NINT(RInputHKLs(jnd,:)))
+      END IF
+    END DO
 
   END SUBROUTINE HKLmake
 
