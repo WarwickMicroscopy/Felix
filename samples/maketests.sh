ba14edeb6da0ab25a25fe55812d2f5166d51f2fb
RudoRoemer <r.roemer@warwick.ac.uk>
2022-12-29 19:29:07 +0000

    now seems to work with the 2 directories

--- samples/maketests.sh
+++ samples/maketests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#EXE=felix.INT64Nifort.d
-EXE=felix.OPT64NGNU.d
+EXE=felix.INT64Nifort.d
+#EXE=felix.OPT64NGNU.d
 
 for dir in GaAs SrTiO3
 do 
@@ -9,7 +9,7 @@ do
     pushd $dir
     pwd
     ../../src/$EXE
-    diff -s -W132 GaAs_Sim_0930A_080x080/ sample_outputs/
+    diff -s -W132 $dir_Sim_*/ 
     popd
 done
 
